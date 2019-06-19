821b98a8876a81a51ea65f63958151ac52bcdd4d
Jayashankar <jayashanakar.boya@byjus.com>
2019-06-19 15:40:54 +0530

    Addressed Review comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,9 @@ buildscript {
 
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.3.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}")
-        classpath("com.google.gms:google-services:4.2.0")
+        classpath(BuildPlugins.android)
+        classpath(BuildPlugins.kotlin)
+        classpath(BuildPlugins.googleServices)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -1,170 +1,174 @@
 object Versions {
 
     // framework config
-    val compileSdkVersion = 28
-    val minSdkVersion = 21
-    val targetSdkVersion = 28
+    const val compileSdkVersion = 28
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 28
 
     // language
-    val kotlinVersion = "1.3.21"
+    const val kotlinVersion = "1.3.21"
 
     // dependency injection
-    val koinVersion = "1.0.2"
+    const val koinVersion = "1.0.2"
 
     // logging
-    val timberVersion = "4.7.1"
+    const val timberVersion = "4.7.1"
 
     // ktx
-    val ktxVersion = "1.1.0-alpha04"
+    const val ktxVersion = "1.2.0-alpha02"
 
     // architecture components
-    val lifecycleVersion = "2.0.0"
-    val coreTestVersion = "1.0.0"
+    const val lifecycleVersion = "2.0.0"
+    const val coreTestVersion = "1.0.0"
 
     // room
-    val roomVersion = "2.1.0-alpha03"
+    const val roomVersion = "2.1.0-alpha03"
 
     // design
-    val appCompatVersion = "1.1.0-alpha02"
-    val materialVersion = "1.1.0-alpha04"
-    val fragmentVersion = "1.0.0"
-    val constraintVersion = "2.0.0-alpha2"
-    val skeletonVersion = "1.1.2"
-    val shimmerVersion = "2.1.0"
+    const val appCompatVersion = "1.0.0"
+    const val materialVersion = "1.1.0-alpha04"
+    const val fragmentVersion = "1.0.0"
+    const val constraintVersion = "2.0.0-beta2"
+    const val skeletonVersion = "1.1.2"
+    const val shimmerVersion = "2.1.0"
 
     // navigation component
-    val navigationVersion = "1.0.0-rc02"
+    const val navigationVersion = "1.0.0-rc02"
 
     // work manager
-    val workVersion = "1.0.0"
+    const val workVersion = "1.0.0"
 
     // reactive extensions
-    val rxJavaVersion = "2.2.7"
-    val rxKotlinVersion = "2.3.0"
-    val rxAndroidVersion = "2.1.1"
+    const val rxJavaVersion = "2.2.7"
+    const val rxKotlinVersion = "2.3.0"
+    const val rxAndroidVersion = "2.1.1"
 
     // network image
-    val glideVersion = "4.9.0"
+    const val glideVersion = "4.9.0"
 
     // epoxy
-    val epoxyVersion = "3.1.0"
+    const val epoxyVersion = "3.1.0"
 
     // lottie
-    val lottieVersion = "2.8.0"
+    const val lottieVersion = "2.8.0"
 
     // debug data base
-    val debugDbVersion = "1.0.4"
+    const val debugDbVersion = "1.0.4"
 
     // firebase
-    val firestoreVersion = "18.1.0"
+    const val firestoreVersion = "18.1.0"
 
     // test
-    val jUnitVersion = "4.12"
-    val testRunnerVersion = "1.1.2-alpha01"
-    val espressoVersion = "3.1.0-beta02"
-    val mockitoVersion = "2.0.0-RC3"
-    val robolectricVersion = "4.1"
+    const val jUnitVersion = "4.12"
+    const val testRunnerVersion = "1.2.0"
+    const val espressoVersion = "3.2.0"
+    const val mockitoVersion = "2.0.0-RC3"
+    const val robolectricVersion = "4.1"
+
+    const val gradlePlugin = "3.3.2"
+    const val googleServices = "4.2.0"
 }
 
+object BuildPlugins {
+    const val android = "com.android.tools.build:gradle:${Versions.gradlePlugin}"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
+    const val googleServices = "com.google.gms:google-services:${Versions.googleServices}"
+}
 
-object appDependencies {
-    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlinVersion}"
-    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
-    val material = "com.google.android.material:material:${Versions.materialVersion}"
-    val koin = "org.koin:koin-android:${Versions.koinVersion}"
-    val koinViewModel = "org.koin:koin-android-viewmodel:${Versions.koinVersion}"
-    val timber = "com.jakewharton.timber:timber:${Versions.timberVersion}"
-    val navFragment = "android.arch.navigation:navigation-fragment-ktx:${Versions.navigationVersion}"
-    val navUi = "android.arch.navigation:navigation-ui-ktx:${Versions.navigationVersion}"
-    val fragment = "androidx.fragment:fragment-ktx:${Versions.fragmentVersion}"
-    val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroidVersion}"
-    val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
-    val archRuntime = "androidx.arch.core:core-runtime:${Versions.lifecycleVersion}"
-    val archExtension = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleVersion}"
-    val archCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycleVersion}"
-    val constraint = "androidx.constraintlayout:constraintlayout:${Versions.constraintVersion}"
-    val ktx = "androidx.core:core-ktx:${Versions.ktxVersion}"
-    val glide = "com.github.bumptech.glide:glide:${Versions.glideVersion}"
-    val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glideVersion}"
-    val epoxy = "com.airbnb.android:epoxy:${Versions.epoxyVersion}"
-    val epoxyProcessor = "com.airbnb.android:epoxy-processor:${Versions.epoxyVersion}"
-    val work = "android.arch.work:work-runtime:${Versions.workVersion}"
-    val lottie = "com.airbnb.android:lottie:${Versions.lottieVersion}"
-    val debugDb = "com.amitshekhar.android:debug-db:${Versions.debugDbVersion}"
-    val skeleton = "com.ethanhua:skeleton:${Versions.skeletonVersion}"
-    val shimmer = "io.supercharge:shimmerlayout:${Versions.shimmerVersion}"
+object AppDependencies {
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlinVersion}"
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
+    const val material = "com.google.android.material:material:${Versions.materialVersion}"
+    const val koin = "org.koin:koin-android:${Versions.koinVersion}"
+    const val koinViewModel = "org.koin:koin-android-viewmodel:${Versions.koinVersion}"
+    const val timber = "com.jakewharton.timber:timber:${Versions.timberVersion}"
+    const val navFragment = "android.arch.navigation:navigation-fragment-ktx:${Versions.navigationVersion}"
+    const val navUi = "android.arch.navigation:navigation-ui-ktx:${Versions.navigationVersion}"
+    const val fragment = "androidx.fragment:fragment-ktx:${Versions.fragmentVersion}"
+    const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroidVersion}"
+    const val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
+    const val archRuntime = "androidx.arch.core:core-runtime:${Versions.lifecycleVersion}"
+    const val archExtension = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleVersion}"
+    const val archCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycleVersion}"
+    const val constraint = "androidx.constraintlayout:constraintlayout:${Versions.constraintVersion}"
+    const val ktx = "androidx.core:core-ktx:${Versions.ktxVersion}"
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glideVersion}"
+    const val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glideVersion}"
+    const val epoxy = "com.airbnb.android:epoxy:${Versions.epoxyVersion}"
+    const val epoxyProcessor = "com.airbnb.android:epoxy-processor:${Versions.epoxyVersion}"
+    const val work = "android.arch.work:work-runtime:${Versions.workVersion}"
+    const val lottie = "com.airbnb.android:lottie:${Versions.lottieVersion}"
+    const val debugDb = "com.amitshekhar.android:debug-db:${Versions.debugDbVersion}"
+    const val skeleton = "com.ethanhua:skeleton:${Versions.skeletonVersion}"
+    const val shimmer = "io.supercharge:shimmerlayout:${Versions.shimmerVersion}"
 
 }
 
-object appTestDependencies {
-    val jUnit = "junit:junit:${Versions.jUnitVersion}"
-    val testRunner = "androidx.test:runner:${Versions.testRunnerVersion}"
-    val espresso = "androidx.test.espresso:espresso-core:${Versions.espressoVersion}"
-    val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
-    val workTest = "android.arch.work:work-testing:${Versions.workVersion}"
+object AppTestDependencies {
+    const val jUnit = "junit:junit:${Versions.jUnitVersion}"
+    const val testRunner = "androidx.test:runner:${Versions.testRunnerVersion}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espressoVersion}"
+    const val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
+    const val workTest = "android.arch.work:work-testing:${Versions.workVersion}"
 }
 
-object presentationDependencies {
-    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlinVersion}"
-    val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
-    val archRuntime = "androidx.arch.core:core-runtime:${Versions.lifecycleVersion}"
-    val archExtension = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleVersion}"
-    val archCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycleVersion}"
-    val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroidVersion}"
-    val timber = "com.jakewharton.timber:timber:${Versions.timberVersion}"
+object PresentationDependencies {
+    const val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
+    const val archRuntime = "androidx.arch.core:core-runtime:${Versions.lifecycleVersion}"
+    const val archExtension = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleVersion}"
+    const val archCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycleVersion}"
+    const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroidVersion}"
+    const val timber = "com.jakewharton.timber:timber:${Versions.timberVersion}"
 }
 
-object presentationTestDependencies {
-    val jUnit = "junit:junit:${Versions.jUnitVersion}"
-    val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
-    val kotlinJUnit = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlinVersion}"
-    val archTest = "androidx.arch.core:core-testing:${Versions.lifecycleVersion}"
+object PresentationTestDependencies {
+    const val jUnit = "junit:junit:${Versions.jUnitVersion}"
+    const val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
+    const val kotlinJUnit = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlinVersion}"
+    const val archTest = "androidx.arch.core:core-testing:${Versions.lifecycleVersion}"
 }
 
-object domainDependencies {
-    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlinVersion}"
-    val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
+object DomainDependencies {
+    const val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
 }
 
-object domainTestDependencies {
-    val jUnit = "junit:junit:${Versions.jUnitVersion}"
-    val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
+object DomainTestDependencies {
+    const val jUnit = "junit:junit:${Versions.jUnitVersion}"
+    const val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
 }
 
-object dataDependencies {
-    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlinVersion}"
-    val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
+object DataDependencies {
+    const val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
 }
 
-object dataTestDependencies {
-    val jUnit = "junit:junit:${Versions.jUnitVersion}"
-    val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
-    val kotlinJUnit = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlinVersion}"
+object DataTestDependencies {
+    const val jUnit = "junit:junit:${Versions.jUnitVersion}"
+    const val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
+    const val kotlinJUnit = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlinVersion}"
 }
 
-object cacheDependencies {
-    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlinVersion}"
-    val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
-    val room = "androidx.room:room-runtime:${Versions.roomVersion}"
-    val roomCompiler = "androidx.room:room-compiler:${Versions.roomVersion}"
-    val roomRx = "androidx.room:room-rxjava2:${Versions.roomVersion}"
+object CacheDependencies {
+    const val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
+    const val room = "androidx.room:room-runtime:${Versions.roomVersion}"
+    const val roomCompiler = "androidx.room:room-compiler:${Versions.roomVersion}"
+    const val roomRx = "androidx.room:room-rxjava2:${Versions.roomVersion}"
 }
 
-object cacheTestDependencies {
-    val jUnit = "junit:junit:${Versions.jUnitVersion}"
-    val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
-    val roomTest = "androidx.room:room-testing:${Versions.roomVersion}"
-    val robolectric = "org.robolectric:robolectric:${Versions.robolectricVersion}"
-    val coreTest = "androidx.test:core:${Versions.coreTestVersion}"
+object CacheTestDependencies {
+    const val jUnit = "junit:junit:${Versions.jUnitVersion}"
+    const val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
+    const val roomTest = "androidx.room:room-testing:${Versions.roomVersion}"
+    const val robolectric = "org.robolectric:robolectric:${Versions.robolectricVersion}"
+    const val coreTest = "androidx.test:core:${Versions.coreTestVersion}"
 }
 
-object firebaseDependencies {
-    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlinVersion}"
-    val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
-    val firestore = "com.google.firebase:firebase-firestore:${Versions.firestoreVersion}"
+object FirebaseDependencies {
+    const val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlinVersion}"
+    const val firestore = "com.google.firebase:firebase-firestore:${Versions.firestoreVersion}"
 }
 
-object firebaseTestDependencies {
-    val jUnit = "junit:junit:${Versions.jUnitVersion}"
-    val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
+object FirebaseTestDependencies {
+    const val jUnit = "junit:junit:${Versions.jUnitVersion}"
+    const val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoVersion}"
 }
+
